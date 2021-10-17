f5a666ac0ade36777c8f7cecc662bbf7617c444f
Giacomo Baso <ganondolf@gmail.com>
2021-10-17 15:15:27 +0200

    add application

---
+++ src/main/java/com/github/gbaso/newtonfractals/NewtonFractalsApplication.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.newtonfractals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtonFractalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtonFractalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gbaso/newtonfractals/NewtonFractalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.newtonfractals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewtonFractalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
