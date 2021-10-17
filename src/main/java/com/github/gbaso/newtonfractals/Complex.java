dc170457bcc83cf497327b2a38351de375da7082
Giacomo Baso <ganondolf@gmail.com>
2021-10-17 15:16:41 +0200

    add complex numbers

---
+++ src/main/java/com/github/gbaso/newtonfractals/Complex.java
@@ -0,0 +1,58 @@
+package com.github.gbaso.newtonfractals;
+
+public record Complex(double real, double imaginary) {
+
+    public static final Complex ZERO = new Complex(0, 0);
+    public static final Complex ONE = new Complex(1, 0);
+    public static final Complex I = new Complex(0, 1);
+
+    private double abs2() {
+        return real * real + imaginary * imaginary;
+    }
+
+    public double abs() {
+        return Math.sqrt(abs2());
+    }
+
+    public Complex negate() {
+        return new Complex(-real, -imaginary);
+    }
+
+    public Complex conjugate() {
+        return new Complex(real, -imaginary);
+    }
+
+    public Complex reciprocal() {
+        return conjugate().divide(abs2());
+    }
+
+    public Complex add(Complex other) {
+        return new Complex(this.real + other.real, this.imaginary + other.imaginary);
+    }
+
+    public Complex subtract(Complex other) {
+        return this.add(other.negate());
+    }
+
+    public Complex multiply(double factor) {
+        return new Complex(real * factor, imaginary * factor);
+    }
+
+    public Complex divide(double divisor) {
+        return new Complex(real / divisor, imaginary / divisor);
+    }
+
+    public Complex multiply(Complex other) {
+        return new Complex(this.real * other.real - this.imaginary * other.imaginary, this.real * other.imaginary + other.real * this.imaginary);
+    }
+
+    public Complex divide(Complex other) {
+        return this.multiply(other.reciprocal());
+    }
+
+    @Override
+    public String toString() {
+        return "(" + real + ", " + imaginary + ")";
+    }
+
+}
---
+++ src/test/java/com/github/gbaso/newtonfractals/commons/CommonsComplexTests.java
@@ -0,0 +1,159 @@
+package com.github.gbaso.newtonfractals.commons;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.commons.math3.complex.Complex;
+import org.assertj.core.api.AbstractDoubleAssert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CommonsComplexTests {
+
+    @Test
+    void string() {
+        assertThat(Complex.ZERO).hasToString("(0.0, 0.0)");
+        assertThat(Complex.ONE).hasToString("(1.0, 0.0)");
+        assertThat(Complex.I).hasToString("(0.0, 1.0)");
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            0,  0
+            1,  0
+            0,  1
+            1,  1
+            """)
+    void components(double x, double y) {
+        var z = new Complex(x, y);
+        assertThat(z.getReal()).isEqualTo(x);
+        assertThat(z.getImaginary()).isEqualTo(y);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+                    0,          0,          0
+                    1,          0,          1
+                    0,          1,          1
+                    0.7071068,  0.7071068,  1
+                    3,          4,          5
+                    -2,         -7,         7.2801099
+            """)
+    void abs(double x, double y, double expected) {
+        var z = new Complex(x, y);
+        assertThat(z.abs() - expected).isLessThan(0.00001);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            0,  0
+            1,  0
+            0,  1
+            1,  1
+            """)
+    void negate(double x, double y) {
+        var z = new Complex(x, y);
+        var w = z.negate();
+        assertThat(w.getReal()).isEqualTo(-x);
+        assertThat(w.getImaginary()).isEqualTo(-y);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            0,  0,  1,  0,  1,  0
+            1,  0,  1,  1,  2,  1
+            0,  1,  1,  0,  1,  1
+            1,  1,  1,  1,  2,  2
+            """)
+    void add(double x1, double y1, double x2, double y2, double rx, double ry) {
+        var z1 = new Complex(x1, y1);
+        var z2 = new Complex(x2, y2);
+        var expected = new Complex(rx, ry);
+        assertThat(z1.add(z2)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            0,  0,  1,  0,  1,  0
+            1,  0,  1,  1,  2,  1
+            0,  1,  1,  0,  1,  1
+            1,  1,  1,  1,  2,  2
+            """)
+    void aboutEquals(double x1, double y1, double x2, double y2, double rx, double ry) {
+        var z1 = new Complex(x1, y1);
+        var z2 = new Complex(x2, y2);
+        var expected = new Complex(rx, ry);
+        assertAboutEquals(z1.add(z2), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+                    0,          0,          0,          0
+                    1,          0,          1,          0
+                    0,          1,          -1,         0
+                    0.7071068,  0.7071068,  0,          1
+                    3,          4,          -7,         24
+                    -2,         -7,         -45,        28
+            """)
+    void square(double x, double y, double rx, double ry) {
+        var z = new Complex(x, y);
+        var expected = new Complex(rx, ry);
+        assertAboutEquals(z.multiply(z), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+                    1,          0,          1,          0
+                    0,          1,          0,          -1
+                    0.7071068,  0.7071068,  0.7071068,  -0.7071068
+                    3,          4,          0.12,        -0.16
+                    -2,         -7,         -0.037736,  0.13207548
+            """)
+    void reciprocal(double x, double y, double rx, double ry) {
+        var z = new Complex(x, y);
+        var expected = new Complex(rx, ry);
+        assertAboutEquals(z.reciprocal(), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+                    0,          0,          0,          0,          0
+                    0,          0,          1,          0,          0
+                    1,          0,          0,          0,          0
+                    1,          0,          1,          1,          0
+                    0,          1,          0,          0,          0
+                    0,          1,          1,          0,          1
+                    0.7071068,  0.7071068,  1.4142135,  1,          1
+                    3,          4,          -2,         -6,         -8
+                    -2,         -7,         0.2,        -0.4,       -1.4
+            """)
+    void multiply(double x, double y, double factor, double rx, double ry) {
+        var z = new Complex(x, y);
+        var expected = new Complex(rx, ry);
+        assertAboutEquals(z.multiply(factor), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            0,  0,  1,  0,  0,          0
+            1,  0,  1,  1,  0.5,        -0.5
+            0,  1,  1,  0,  0,          1
+            1,  1,  1,  1,  1,          0
+            3,  4,  -2, -7, -0.6415094, 0.2452830
+            """)
+    void divide(double x1, double y1, double x2, double y2, double rx, double ry) {
+        var z1 = new Complex(x1, y1);
+        var z2 = new Complex(x2, y2);
+        var expected = new Complex(rx, ry);
+        assertAboutEquals(z1.divide(z2), expected);
+    }
+
+    AbstractDoubleAssert<?> assertAboutEquals(Complex actual, Complex expected) {
+        return assertThat(diff(actual, expected)).isLessThan(0.0001);
+    }
+
+    double diff(Complex z1, Complex z2) {
+        return z1.subtract(z2).abs();
+    }
+
+}
